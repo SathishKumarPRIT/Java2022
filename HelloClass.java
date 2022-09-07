c8961bfed88a5e6c885df60970e5f17c049cc9a0
SathishKumarPRIT <111410960+SathishKumarPRIT@users.noreply.github.com>
2022-09-06 23:26:45 -0700

    version1

---
+++ HelloClass.java
@@ -0,0 +1,16 @@
+package one;
+
+public class A {
+
+
+ A getA(){ 
+return this; 
+} 
+void msg(){System.out.println("Hello java");} 
+} 
+
+class Test1{ 
+	public static void main(String args[]){ 
+ new A().getA().msg(); 
+ } 
+ } 
